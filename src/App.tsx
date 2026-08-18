f7a71acc9d3774ffbb67add3b49bc99af62cbc62
Juliana Vitoria <julianaenob@gmail.com>
2026-08-18 10:05:30 -0300

    💄 UI:  Criacao de Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+
+
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <Home />
-            <Footer />
-        </>
-    );
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cadastro' element={<Cadastro />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App;
+export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
             <div className="container flex justify-between text-lg mx-8">
-                Blog Pessoal
+               
+               <Link to="/" className="text-2zl font-bold">  Blog Pessoal </Link>
 
                 <div className="flex gap-4">
                     Postagens
                     Temas
                     Cadastrar tema
                     Perfil
-                    Sair
+                    <Link to="/login" className="hover:underline" > Sair </Link>
                 </div>
             </div>
         </div>

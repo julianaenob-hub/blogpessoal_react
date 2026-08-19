524ad04694b62873aba45b0a2f335ca6074a9cf9
Juliana Vitoria <julianaenob@gmail.com>
2026-08-19 16:58:43 -0300

    💥 NEW: Cadastro e Login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,36 @@
-import { Link } from "react-router-dom"
-
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+ 
 function Navbar() {
+    const navigate = useNavigate();
+ 
+    const { handleLogout } = useContext(AuthContext);
+    function logout() {
+        handleLogout();
+        alert("Usuário desconectado com sucesso!");
+        navigate("/");
+    }
+ 
     return (
-        <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
-            <div className="container flex justify-between text-lg mx-8">
-               
-               <Link to="/" className="text-2zl font-bold">  Blog Pessoal </Link>
-
-                <div className="flex gap-4">
-                    Postagens
-                    Temas
-                    Cadastrar tema
-                    Perfil
-                    <Link to="/login" className="hover:underline" > Sair </Link>
+        <>
+            <div className='w-full flex justify-center py-4
+                            bg-indigo-900 text-white'>
+ 
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to="/" className="text-2xl font-bold">Blog Pessoal </Link>
+ 
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        <Link onClick={logout} to="" className="hover:underline">Sair </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
-
+ 
 export default Navbar

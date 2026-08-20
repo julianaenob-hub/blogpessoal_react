69a7dfb3079a9bdb8a4d3380203342e3db824adb
Juliana Vitoria <julianaenob@gmail.com>
2026-08-20 16:40:34 -0300

    💥 NEW: Construcao Tema

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,44 +3,51 @@ import { Link, useNavigate } from "react-router-dom";
 import type UsuarioLogin from "../../models/UsuarioLogin";
 import { AuthContext } from "../../contexts/AuthContext";
 import { ClipLoader } from "react-spinners";
- 
+
 function Login() {
- 
+
     const navigate = useNavigate();
- 
+
     const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>({} as UsuarioLogin);
- 
+
     // Consumir os estados e funções da context (AuthContext)
     // usando o hook useContext (Consumer)
     const { usuario, handleLogin, isLoading } = useContext(AuthContext);
+
     useEffect(() => {
         if (usuario.token !== "") {
             navigate("/home");
         }
-    }, [usuario])
- 
+    }, [usuario]);
+
     function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
         setUsuarioLogin({
             ...usuarioLogin,
             [e.target.name]: e.target.value,
-        })
+        });
     }
- 
+
     function login(e: SyntheticEvent<HTMLFormElement>) {
         e.preventDefault();
         handleLogin(usuarioLogin);
     }
- 
+
     console.log(JSON.stringify(usuarioLogin));
+
     return (
         <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4"
+            <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
+
+                <form
+                    className="flex justify-center items-center flex-col w-1/2 gap-4"
                     onSubmit={login}
                 >
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
+
+                    <h2 className="text-slate-900 text-5xl">Entrar</h2>
+
                     <div className="flex flex-col w-full">
                         <label htmlFor="usuario">Usuário</label>
+
                         <input
                             type="text"
                             id="usuario"
@@ -51,8 +58,10 @@ function Login() {
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
+
                     <div className="flex flex-col w-full">
                         <label htmlFor="senha">Senha</label>
+
                         <input
                             type="password"
                             id="senha"
@@ -63,38 +72,46 @@ function Login() {
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
-                    {
-                        isLoading ? (
-                            <ClipLoader
-                                color="#ffffff"
-                                size={24}
-                            />
-                        ) : (
-                            <span>Entrar</span>
-                        )
-                    }
+
                     <button
-                        type='submit'
+                        type="submit"
                         className="rounded bg-indigo-400 flex justify-center
-                                   hover:bg-indigo-900 text-white w-1/2 py-2">
-                        <span>Entrar</span>
+                                   hover:bg-indigo-900 text-white w-1/2 py-2"
+                    >
+                        {
+                            isLoading ? (
+                                <ClipLoader
+                                    color="#ffffff"
+                                    size={24}
+                                />
+                            ) : (
+                                <span>Entrar</span>
+                            )
+                        }
                     </button>
- 
+
                     <hr className="border-slate-800 w-full" />
- 
+
                     <p>
                         Ainda não tem uma conta?{' '}
-                        <Link to="/cadastro" className="text-indigo-800 hover:underline">
+                        <Link
+                            to="/cadastro"
+                            className="text-indigo-800 hover:underline"
+                        >
                             Cadastre-se
                         </Link>
                     </p>
+
                 </form>
-                <div className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat
-                            w-full min-h-screen bg-cover bg-center"
+
+                <div
+                    className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat
+                               w-full min-h-screen bg-cover bg-center"
                 ></div>
+
             </div>
         </>
     );
 }
- 
+
 export default Login;
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,17 +1,49 @@
 import axios from "axios";
-
+ 
+// Conectar com o backend (baseURL = endereço Swagger)
 const api = axios.create({
-  baseURL: " https://blogpessoall-lfng.onrender.com"
+    baseURL: 'https://blogpessoall-lfng.onrender.com'
 })
-
-// Função Cadastrar Usuário
-export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) =>{
-  const resposta = await api.post(url, dados)
-  setDados(resposta.data)
+ 
+// Criar funções para enviar requisições ao backend
+// 1. Função Cadastrar usuário:
+export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
 }
-
-// Função Autenticar Usuário
+// A função setDados vai atualizar o meu estado com os dados recebidos da api (ou seja, o ID - que vem da api e não é o usuário que escreve)
+// .data é o corpo da resposta
+ 
+// 2. Função Autenticar usuário:
 export const login = async (url: string, dados: Object, setDados: Function) => {
-  const resposta = await api.post(url, dados)
-  setDados(resposta.data)
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
 }
+ 
+// COM TOKENS
+// Função Consultar com token
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header);
+    setDados(resposta.data);
+}
+ 
+// Função Cadastrar com token
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+ 
+ 
+// Função Atualizar com token
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+ 
+ 
+// Função deletar com token
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header);
+}
+ 
+ 
